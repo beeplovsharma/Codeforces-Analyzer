bf60a273c9c1582d7c9c543426d621d0dde5fa6d
Beeplov Sharma <beeplovsharma@gmail.com>
2024-08-02 10:17:15 +0530

    cf analyzer

--- src/pages/SingleUser.js
+++ src/pages/SingleUser.js
@@ -1,4 +1,4 @@
-import { useState, useEffect } from 'react';
+import { useState } from 'react';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import axios from 'axios'
